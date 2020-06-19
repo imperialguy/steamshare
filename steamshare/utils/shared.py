8c1095e3a55a58467285bbed825b0f68f9652314
Ven Karri <karri.ven@gmail.com>
2020-06-18 20:00:13 -0400

    STEAM-12: added nested dictionary and steam thread

--- steamshare/utils/shared.py
+++ steamshare/utils/shared.py
@@ -8,12 +8,60 @@ from flask import jsonify
 from dateutil import tz
 import datetime
 import tornado.ioloop
+import threading
 import tempfile
 import logging
+import inspect
 import pandas
 import random
+import ctypes
+import trio
 import sys
 import os
+
+
+class NestedDict(dict):
+    """Nested Dictionary class
+
+    """
+
+    def __getitem__(self, item):
+        try:
+            return dict.__getitem__(self, item)
+        except KeyError:
+            value = self[item] = type(self)()
+            return value
+
+class SteamThread(threading.Thread):
+    def _get_my_tid(self):
+        """determines this (self's) thread id"""
+        if not self.isAlive():
+            raise threading.ThreadError("the thread is not active")
+
+        # do we have it cached?
+        if hasattr(self, "_thread_id"):
+            return self._thread_id
+
+        # no, look for it in the _active dict
+        for tid, tobj in threading._active.items():
+            if tobj is self:
+                self._thread_id = tid
+                return tid
+
+        raise AssertionError("could not determine the thread's id")
+
+    def raise_exc(self, exctype):
+        """raises the given exception type in the context of this thread"""
+        # StaticShared._async_raise(self._get_my_tid(), exctype)
+        if not self.isAlive():
+            raise threading.ThreadError("the thread is not active")
+        # print('Thread id: {} name: {}'.format(self.ident, self.name))
+        StaticShared._async_raise(self.ident, exctype)
+
+    def terminate(self):
+        """raises SystemExit in the context of the given thread, which should
+        cause the thread to exit silently (unless caught)"""
+        self.raise_exc(SystemExit)
 
 
 class StaticShared(object):
@@ -24,6 +72,24 @@ class StaticShared(object):
         os.environ.update(params)
         yield
         os.environ = orig
+
+    @staticmethod
+    def _async_raise(tid, exctype):
+        """raises the exception, performs cleanup if needed"""
+        if not inspect.isclass(exctype):
+            raise TypeError("Only types can be raised (not instances)")
+        # res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(
+        #     exctype))
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(tid),
+            ctypes.py_object(exctype))
+        if res == 0:
+            raise ValueError("invalid thread id")
+        elif res != 1:
+            # """if it returns a number greater than one, you're in trouble,
+            # and you should call it again with exc=NULL to revert the effect"""
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, 0)
+            raise SystemError("PyThreadState_SetAsyncExc failed")
+
 
     @staticmethod
     def get_logger(filename,
